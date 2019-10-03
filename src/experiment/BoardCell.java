68f44f9905852747fb56cb8887fe5bbe22d0e947
Ruidi Huang <ruidihuang@mines.edu>
2019-10-03 12:16:13 -0600

    Added some functions to BoardCell and InBoard classes

--- src/experiment/BoardCell.java
+++ src/experiment/BoardCell.java
@@ -4,6 +4,12 @@ public class BoardCell {
 	private int row;
 	private int col;
 	
+	public BoardCell(int row, int col) {
+		super();
+		this.row = row;
+		this.col = col;
+	}
+	
 	
 	
 }
--- src/experiment/IntBoard.java
+++ src/experiment/IntBoard.java
@@ -1,5 +1,6 @@
 package experiment;
 
+import java.util.*;
 import java.util.Map;
 import java.util.Set;
 
@@ -9,8 +10,8 @@ public class IntBoard {
 	private Map<BoardCell, Set<BoardCell>> adjMtx;
 	private Set<BoardCell> visited;
 	private Set<BoardCell> targets;
-	private final static int ROW = 5;
-	private final static int COL = 5;
+	private final static int ROW = 4;
+	private final static int COL = 4;
 	
 	
 	public IntBoard() {
@@ -20,19 +21,42 @@ public class IntBoard {
 	}
 	
 	public void calcAdjacencies() {
-		for (int i = 0; i < ROW; i++) {
-			for (int j = 0; j < COL; j++) {
-				
+		Set<BoardCell> tempSet = new HashSet<BoardCell>();
+		for (int i = 0; i < ROW+1; i++) {
+			for (int j = 0; j < COL+1; j++) {
+				BoardCell temp = new BoardCell(i,j);
+				for (int a = -1; a <= 1; a++) {
+					for (int b = -1; b <= 1; b++) {
+						if (i + a >= 0 || i + a <= ROW) {
+							if (j + b >= 0 || j + b <= COL) {
+								if (Math.abs(a) == Math.abs(b)) {
+									BoardCell tempT = new BoardCell(i + a,j + b);
+									tempSet.add(tempT);
+								}
+							}
+						}
+					}
+				}
+				adjMtx.put(temp, tempSet);
+				tempSet.clear();
 			}
 		}
 	}
 	
-	public void getAdList() {
-		
+	public Set<BoardCell> getAdList(int row, int col) {
+		BoardCell temp = new BoardCell(row,col);
+		return adjMtx.get(temp);
 	}
 	
-	public void calcTargets() {
-		
+	public void calcTargets(BoardCell startCell, int pathLength) {
+		for (int i = 0; i < ROW + 1; i++) {
+			for (int j = 0; j < COL + 1; j++) {
+				if (i + j == pathLength) {
+					BoardCell temp = new BoardCell(i,j);
+					targets.add(temp);
+				}
+			}
+		}
 	}
 	
 	public void getTargets() {
