e6a2be440b10fb8f9fc1d2defd612ff754e781ad
Mehmet <mehmet.mhy@gmail.com>
2019-10-16 18:40:41 -0600

    Update BadConfigFormatException.java

--- src/clueGame/BadConfigFormatException.java
+++ src/clueGame/BadConfigFormatException.java
@@ -1,5 +1,17 @@
 package clueGame;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.text.NumberFormat;
 
 public class BadConfigFormatException {
+	
+	public BadConfigFormatException() {
+		super();
+	}
 
+	@Override
+	public String toString() {
+		return null;
+	}
 }
+
